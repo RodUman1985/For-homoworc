e36227bb1e43e6d02999153130385b3867fff671
RodUman1985 <Vand_85@mail.ru>
2022-07-28 11:25:53 +0300

    ti

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+    String zog= "рак";
+    switch (zog) {
+        case "овен":
+            System.out.println(" 1");
+            break;
+        case "телец":
+            System.out.println(" 2");
+            break;
+        case "близнецы":
+            System.out.println("3 ");
+            break;
+        case "рак":
+            System.out.println(" 4");
+            break;
+        case "лев":
+            System.out.println(" 5");
+            break;
+        case "дева":
+            System.out.println(" 6");
+            break;
+        case "весы":
+            System.out.println(" 7");
+            break;
+        case "скорпион":
+            System.out.println(" 8");
+            break;
+        case "стрелец":
+            System.out.println("9");
+            break;
+        case "козерог":
+            System.out.println("10");
+            break;
+        case "водолей":
+            System.out.println("11");
+            break;
+        case "рыбы":
+            System.out.println("12");
+            break;
+    }
+
+    }
+
+
+}
